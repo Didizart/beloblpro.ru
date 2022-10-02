a8ef7e4330fd0cb6a3306e173362016bf3144425
Didizart <114870752+Didizart@users.noreply.github.com>
2022-10-02 18:46:38 +0300

    Initial commit

---
+++ wp-content/uploads/js-cache-9bd2.js
@@ -0,0 +1,13 @@
+    (function($){
+    $(document).ready(function() {
+
+        
+        $('.parallax').each(function(){
+           $(this).parallax("50%", 0.4);
+        });
+
+        
+        
+    });
+    })(jQuery);
+     /*2020-02-03 08:00*//* JS Generator Execution Time: 7.8201293945312E-5 seconds */
